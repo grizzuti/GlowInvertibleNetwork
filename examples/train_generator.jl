8db767f0f2e55440cad07079742cb10148b875c4
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2021-10-22 15:44:58 +0200

    Modified training procedure

--- examples/train_generator.jl
+++ examples/train_generator.jl
@@ -47,18 +47,20 @@ nepochs = 2^9
 lr = 1f-4
 lr_min = lr*1f-2
 decay_rate = exp(log(lr_min/lr)/(nepochs*nbatches))
-grad_max = lr*1f7
-opt = Optimiser(ClipNorm(grad_max), ExpDecay(lr, decay_rate, 1, lr_min), ADAM(lr))
+# grad_max = lr*1f7
+# opt = Optimiser(ClipNorm(grad_max), ExpDecay(lr, decay_rate, 1, lr_min), ADAM(lr))
+λ = 1f-2
+opt = Optimiser(ExpDecay(lr, decay_rate, 1, lr_min), ADAM(lr))
 intermediate_save = 1
 
 # Training
+G.forward(gpu(X[:,:,:,randperm(ntrain)[1:batch_size]])) # to initialize actnorm
+θ = get_params(G)
+θ_backup = deepcopy(θ)
 floss = zeros(Float32, nbatches, nepochs)
 floss_full = zeros(Float32, nbatches, nepochs)
 Ztest = randn(Float32, nx,ny,1,batch_size)
 for e = 1:nepochs # epoch loop
-
-    # Set backup state
-    θ_backup = get_params(G) |> cpu
 
     # Select random data traversal for current epoch
     idx_e = reshape(randperm(ntrain), batch_size, nbatches)
@@ -79,29 +81,34 @@ for e = 1:nepochs # epoch loop
         floss[b,e], ΔZb = loss(Zb)
         floss_full[b,e] = floss[b,e]-lgdt
 
+        # Epoch-adaptive regularization weight
+        λ_adaptive = λ*floss[1,e]/(0.5f0*norm(θ_backup)^2)
+
+        # Computing gradient
+        G.backward(ΔZb, Zb)
+
+        # Update params + regularization
+        for i =1:length(θ)
+            Δθ = θ[i].data-θ_backup[i].data
+            update!(opt, θ[i].data, θ[i].grad+λ_adaptive*Δθ)
+            floss_full[b,e] += 0.5f0*λ_adaptive*norm(Δθ)^2
+            (b == nbatches) && (θ_backup[i].data .= θ[i].data)
+        end
+
         # Print current iteration
         print("Iter: epoch=", e, "/", nepochs, ", batch=", b, "/", nbatches, "; f_full = ", floss_full[b,e], "; f_z = ", floss[b,e], "\n")
 
         # Check instability status
         if isnan(floss_full[b,e]) || isinf(floss_full[b,e])
-            set_params!(G, gpu(θ_backup))
-            save(save_intermediate_filename, "theta", cpu(get_params(G)), "floss_full", floss_full, "floss", floss)
+            set_params!(G, θ_backup)
+            save(save_intermediate_filename, "theta", cpu(θ), "floss_full", floss_full, "floss", floss)
             throw("NaN or Inf values!\n")
-        end
-
-        # Computing gradient
-        G.backward(ΔZb, Zb)
-
-        # Update params
-        for p in get_params(G)
-            update!(opt, p.data, p.grad)
-            p.grad = nothing # clear gradient
         end
 
     end # end batch loop
 
     # Saving and plotting intermediate results
-    save(save_intermediate_filename, "theta", cpu(get_params(G)), "floss_full", floss_full, "floss", floss)
+    save(save_intermediate_filename, "theta", cpu(θ), "floss_full", floss_full, "floss", floss)
     if mod(e, intermediate_save) == 0
         X_test = G.inverse(gpu(Ztest)) |> cpu
         plot_image(X_test[:, :, 1, 1]; figsize=(5,5), vmin=min(X_test[:,:,1,1]...), vmax=max(X_test[:,:,1,1]...), title=L"$\mathbf{x}$", path=string(save_folder, "new_samples_intermediate_",nx,"x", ny,".png"))
--- src/invertible_layer_conv1x1orth_fixed.jl
+++ src/invertible_layer_conv1x1orth_fixed.jl
@@ -3,8 +3,6 @@ export Conv1x1orth_fixed
 struct Conv1x1orth_fixed{T<:Real} <: InvertibleNetwork
     nc::Int64
     P::AbstractMatrix{T}
-    Pinv::AbstractMatrix{T}
-    p::Parameter
     logdet::Bool
 end
 
@@ -13,8 +11,7 @@ end
 function Conv1x1orth_fixed(nc::Int64; logdet::Bool=true, T::DataType=Float32)
 
     P = Array(qr(randn(T, nc, nc)).Q)
-    Pinv = P\idmat(T, nc)
-    return Conv1x1orth_fixed{T}(nc, P, Pinv, Parameter([T(0)]), logdet)
+    return Conv1x1orth_fixed{T}(nc, P, logdet)
 
 end
 
@@ -25,24 +22,16 @@ function forward(X::AbstractArray{T,4}, C::Conv1x1orth_fixed{T}) where T
 
 end
 
-inverse(Y::AbstractArray{T,4}, C::Conv1x1orth_fixed{T}) where T = return conv1x1(Y, C.Pinv)
+inverse(Y::AbstractArray{T,4}, C::Conv1x1orth_fixed{T}) where T = return conv1x1(Y, toConcreteArray(C.P'))
 
-function backward(ΔY::AbstractArray{T,4}, Y::AbstractArray{T,4}, C::Conv1x1orth_fixed{T}) where T
-
-    # Backpropagating input
-    ΔX = conv1x1(ΔY, toConcreteArray(C.P'))
-    X = inverse(Y, C)
-    C.p.grad = [T(0)]
-    return ΔX, X
-
-end
+backward(ΔY::AbstractArray{T,4}, Y::AbstractArray{T,4}, C::Conv1x1orth_fixed{T}) where T = inverse(ΔY, C), inverse(Y, C)
 
 
 # Other utils
 
 function clear_grad!(::Conv1x1orth_fixed) end
 
-get_params(C::Conv1x1orth_fixed{T}) where T = [C.p]
+get_params(::Conv1x1orth_fixed{T}) where T = [Parameter([T(0)], [T(0)])]
 
-gpu(C::Conv1x1orth_fixed{T}) where T = Conv1x1orth_fixed{T}(C.nc, gpu(C.P), gpu(C.Pinv), gpu(C.p), C.logdet)
-cpu(C::Conv1x1orth_fixed{T}) where T = Conv1x1orth_fixed{T}(C.nc, cpu(C.P), cpu(C.Pinv), cpu(C.p), C.logdet)
+gpu(C::Conv1x1orth_fixed{T}) where T = Conv1x1orth_fixed{T}(C.nc, gpu(C.P), C.logdet)
+cpu(C::Conv1x1orth_fixed{T}) where T = Conv1x1orth_fixed{T}(C.nc, cpu(C.P), C.logdet)
